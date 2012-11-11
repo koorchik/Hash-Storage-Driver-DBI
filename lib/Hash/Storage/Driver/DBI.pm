532a7b29d155783a7d3587785ce30199d9b38613
Viktor Turskyi <viktor@webbylab.com>
2012-11-11 18:17:36 +0200

    Initial commit

---
+++ lib/Hash/Storage/Driver/DBI.pm
@@ -0,0 +1,111 @@
+package Hash::Storage::Driver::DBI;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Hash::Storage::Driver::DBI - The great new Hash::Storage::Driver::DBI!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Hash::Storage::Driver::DBI;
+
+    my $foo = Hash::Storage::Driver::DBI->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Viktor Turskyi, C<< <koorchik at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-hash-storage-driver-dbi at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Hash-Storage-Driver-DBI>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Hash::Storage::Driver::DBI
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Hash-Storage-Driver-DBI>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Hash-Storage-Driver-DBI>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Hash-Storage-Driver-DBI>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Hash-Storage-Driver-DBI/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Viktor Turskyi.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Hash::Storage::Driver::DBI
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+    use_ok( 'Hash::Storage::Driver::DBI' ) || print "Bail out!\n";
+}
+
+diag( "Testing Hash::Storage::Driver::DBI $Hash::Storage::Driver::DBI::VERSION, Perl $], $^X" );
